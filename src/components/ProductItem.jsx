8a33043a75fb713af29b3ece012981e112ae5485
Alice Chu <chienyi.alice@gmail.com>
2023-03-13 16:33:39 +0800

    新增antd+CSS Grid+CSS Module

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,19 @@
+const ProductItem = ({product}) =>{
+    return(
+        <article className="image__layout py-3 py-sm-5">
+        <div className="container">
+
+          <div className="row">
+            <div className="image">
+              <a href="" className="image__link">
+                <img src={product.image} alt="" className="image__style"/>
+              </a>
+              <p className="image_title">{product.name}</p>
+            </div>
+            </div>
+        </div>
+      </article>
+    );
+}
+
+export default ProductItem;
